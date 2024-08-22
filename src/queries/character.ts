9d65b73200f92c27b8a7f063fbe17e4e8e7d2064
Raihan Syah <raihansyah45@gmail.com>
2024-08-23 04:39:34 +0700

    refactor queries into separate file for better organization ;

--- src/queries/character.ts
+++ src/queries/character.ts
@@ -1,28 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GET_EPISODES = gql`
-  query GetEpisodes($page: Int!, $filter: FilterEpisode) {
-    episodes(page: $page, filter: $filter) {
-      results {
-        id
-        name
-        air_date
-        episode
-        characters {
-          id
-          name
-          status
-          species
-          type
-          gender
-          image
-        }
-        created
-      }
-    }
-  }
-`;
-
 export const GET_CHARACTERS = gql`
   query {
     characters {
